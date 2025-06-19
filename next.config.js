5c92501e49d021d358d5a9e561478485cda52d6c
tro3373 <tro3373@gmail.com>
2025-06-19 19:48:39 +0900

    fix(webpack): Fix build error: Add ignore-loader to handle .cursorignore
    - Add:
      - Added ignore-loader to webpack config to ignore
        .cursorignore files. This prevents issues during
        builds caused by these files.
    - Update:
      - Updated package.json and package-lock.json to
        include ignore-loader as a dev dependency.

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  webpack: (config) => {
+    // Ignore .cursorignore and other problematic files during build
+    config.module.rules.push({
+      test: /\.cursorignore$/,
+      use: 'ignore-loader',
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
